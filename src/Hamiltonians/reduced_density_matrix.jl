874122fcb1f733350c7fd4a5a4b14e00d4775252
Satyanand Kuwar <168614969+Skuwar1@users.noreply.github.com>
2024-11-05 13:29:29 +1300

    Update reduced_density_matrix.jl

--- src/Hamiltonians/reduced_density_matrix.jl
+++ src/Hamiltonians/reduced_density_matrix.jl
@@ -145,13 +145,13 @@ j_n> ... > j_{i+1} > j_{i} > ... > j_1 and k_n> ... > k_{i+1} > k_{i} > ... > k_
 * [`SingleParticleExcitation`](@ref)
 * [`TwoParticleExcitation`](@ref)
 """
-struct ReducedDensityMatrix{TT} <: AbstractOperator{TT}
+struct ReducedDensityMatrix <: AbstractOperator{Matrix{Float64}}
     M::Int
     n::Int
 end
 function ReducedDensityMatrix(addr::SingleComponentFockAddress; n = 1)
     M = num_modes(addr)
-    return ReducedDensityMatrix{Float64}(M,n)
+    return ReducedDensityMatrix(M,n)
 end
 function Base.show(io::IO, op::ReducedDensityMatrix)
     print(io, "ReducedDensityMatrix(num_modes = $(op.M), n=$(op.n))")
