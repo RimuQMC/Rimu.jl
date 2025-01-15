9712ee9df92f3a47d8bf95ac93dc20c27f64e94e
jamie-tay <jamiet96@gmail.com>
2025-01-15 14:03:15 +1300

    Update tests and docs with new naming scheme

--- test/excited_states_tests.jl
+++ test/excited_states_tests.jl
@@ -13,9 +13,9 @@ using Test
     p = ProjectorMonteCarloProblem(ham; spectral_strategy, last_step, style)
     s = solve(p)
     df = DataFrame(s)
-    energy1 = shift_estimator(df, shift="shift_s1", skip=1000)
-    energy2 = shift_estimator(df, shift="shift_s2", skip=1000)
-    energy3 = shift_estimator(df, shift="shift_s3", skip=1000)
+    energy1 = shift_estimator(df, shift="shift_r1s1", skip=1000)
+    energy2 = shift_estimator(df, shift="shift_r1s2", skip=1000)
+    energy3 = shift_estimator(df, shift="shift_r1s3", skip=1000)
 
     @test energy1.mean ≈ vals[1]
     @test energy2.mean ≈ vals[2]
@@ -25,12 +25,12 @@ using Test
     p = ProjectorMonteCarloProblem(ham; spectral_strategy, last_step, style, n_replicas)
     s = solve(p)
     df = DataFrame(s)
-    energy1 = shift_estimator(df, shift="shift_1_s1", skip=1000)
-    energy2 = shift_estimator(df, shift="shift_1_s2", skip=1000)
-    energy3 = shift_estimator(df, shift="shift_1_s3", skip=1000)
-    energy4 = shift_estimator(df, shift="shift_2_s1", skip=1000)
-    energy5 = shift_estimator(df, shift="shift_2_s2", skip=1000)
-    energy6 = shift_estimator(df, shift="shift_2_s3", skip=1000)
+    energy1 = shift_estimator(df, shift="shift_r1s1", skip=1000)
+    energy2 = shift_estimator(df, shift="shift_r1s2", skip=1000)
+    energy3 = shift_estimator(df, shift="shift_r1s3", skip=1000)
+    energy4 = shift_estimator(df, shift="shift_r2s1", skip=1000)
+    energy5 = shift_estimator(df, shift="shift_r2s2", skip=1000)
+    energy6 = shift_estimator(df, shift="shift_r2s3", skip=1000)
 
     @test energy1.mean ≈ vals[1]
     @test energy2.mean ≈ vals[2]
@@ -38,4 +38,11 @@ using Test
     @test energy4.mean ≈ vals[1]
     @test energy5.mean ≈ vals[2]
     @test energy6.mean ≈ vals[3]
+
+    replica_strategy = AllOverlaps(n_replicas;mixed_spectral_overlaps=true)
+    p = ProjectorMonteCarloProblem(ham; spectral_strategy, last_step, style, replica_strategy)
+    s = solve(p)
+    df = DataFrame(s)
+    num_overlaps = length(filter(startswith(r"r[0-9]+s[0-9]+_dot"), names(df)))
+    @test num_overlaps == 15
 end
