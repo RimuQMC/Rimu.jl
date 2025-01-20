b432ecaf49d4878b40df0a8879f068f09f6a1d0e
jamie-tay <jamiet96@gmail.com>
2025-01-20 15:51:37 +1300

    Add tests

--- test/excited_states_tests.jl
+++ test/excited_states_tests.jl
@@ -5,14 +5,16 @@ using Test
 @testset "excited state energies" begin
     ham = HubbardReal1D(BoseFS(1,1,1,1,1))
     pr = ExactDiagonalizationProblem(ham)
-    vals = solve(pr).values
+    result = solve(pr)
+    vals = result.values
+    vecs = result.vectors
+    g2s = [rayleigh_quotient(G2RealCorrelator(0),vecs[i]) for i in 1:3]
 
     spectral_strategy = GramSchmidt(3)
     last_step=2000
     style = IsDeterministic()
     p = ProjectorMonteCarloProblem(ham; spectral_strategy, last_step, style)
-    s = solve(p)
-    df = DataFrame(s)
+    df = DataFrame(solve(p))
     energy1 = shift_estimator(df, shift="shift_r1s1", skip=1000)
     energy2 = shift_estimator(df, shift="shift_r1s2", skip=1000)
     energy3 = shift_estimator(df, shift="shift_r1s3", skip=1000)
@@ -23,8 +25,7 @@ using Test
 
     n_replicas = 2
     p = ProjectorMonteCarloProblem(ham; spectral_strategy, last_step, style, n_replicas)
-    s = solve(p)
-    df = DataFrame(s)
+    df = DataFrame(solve(p))
     energy1 = shift_estimator(df, shift="shift_r1s1", skip=1000)
     energy2 = shift_estimator(df, shift="shift_r1s2", skip=1000)
     energy3 = shift_estimator(df, shift="shift_r1s3", skip=1000)
@@ -39,10 +40,13 @@ using Test
     @test energy5.mean ≈ vals[2]
     @test energy6.mean ≈ vals[3]
 
-    replica_strategy = AllOverlaps(n_replicas;mixed_spectral_overlaps=true)
+    replica_strategy = AllOverlaps(n_replicas; operator=G2RealCorrelator(0), mixed_spectral_overlaps=true)
     p = ProjectorMonteCarloProblem(ham; spectral_strategy, last_step, style, replica_strategy)
-    s = solve(p)
-    df = DataFrame(s)
+    df = DataFrame(solve(p))
+    for state in 1:3
+        r = rayleigh_replica_estimator(df; spectral_state=state)
+        @test r.f ≈ g2s[state] atol=0.001
+    end
     num_overlaps = length(filter(startswith(r"r[0-9]+s[0-9]+_dot"), names(df)))
     @test num_overlaps == 15
 end
