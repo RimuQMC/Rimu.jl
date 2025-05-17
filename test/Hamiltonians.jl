889c8b46cf765388c4dc6336133055a431178874
Satyanand Kuwar <168614969+Skuwar1@users.noreply.github.com>
2025-05-17 21:39:54 +1200

    Update Hamiltonians.jl

--- test/Hamiltonians.jl
+++ test/Hamiltonians.jl
@@ -1626,7 +1626,7 @@ end
 
     @testset "interaction" begin
         for H in (
-            ExtendedHubbardReal1D(FermiFS((1,0,1,0)), v=6, t=2.0, interaction = :nearest_neighbour),
+            ExtendedHubbardReal1D(FermiFS((1,0,1,0)), v=6, t=2.0, interaction = :nearest_neighbor),
             ExtendedHubbardReal1D(FermiFS((1,0,1,0)), v=6, t=2.0, interaction = 3)
         )
 
@@ -1636,6 +1636,7 @@ end
             ebhinteraction, bhinteraction = Hamiltonians.extended_hubbard_interaction(H, addr, H.interaction)
             @test diagonal_element(H, addr) == convert(eltype(H),  6 * ebhinteraction)
         end
+        @test_throws ArgumentError ExtendedHubbardReal1D(BoseFS(1,1,1,1); interaction = neaerst_neighbor)
     end
 end
 
