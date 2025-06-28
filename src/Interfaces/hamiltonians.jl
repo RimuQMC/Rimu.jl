6a1306a5d70bb5dd3f75141dde314efb2c150c88
Joachim Brand <joachim.brand@gmail.com>
2025-06-28 18:53:12 +1200

    fix order

--- src/Interfaces/hamiltonians.jl
+++ src/Interfaces/hamiltonians.jl
@@ -339,52 +339,6 @@ Part of the [`AbstractHamiltonian`](@ref) interface.
 """
 starting_address(m::AbstractMatrix) = findmin(real.(diag(m)))[2]
 
-"""
-    offdiagonals(column)
-    offdiagonals(h::AbstractHamiltonian, address) # (deprecated)
-
-Return an iterator over nonzero off-diagonal matrix elements of `h` in the same column as
-`address`. Will iterate over pairs `(newaddress, matrixelement)` or `newaddress => matrixelement`.
-
-# Example
-
-```jldoctest
-julia> address = BoseFS(3,2,1);
-
-
-julia> H = HubbardReal1D(address);
-
-
-julia> h = offdiagonals(H, address)
-6-element Rimu.Hamiltonians.Offdiagonals{BoseFS{6, 3, BitString{8, 1, UInt8}}, Float64, HubbardReal1D{Float64, BoseFS{6, 3, BitString{8, 1, UInt8}}, 1.0, 1.0}}:
- (fs"|2 3 1⟩", -3.0)
- (fs"|2 2 2⟩", -2.449489742783178)
- (fs"|3 1 2⟩", -2.0)
- (fs"|4 1 1⟩", -2.8284271247461903)
- (fs"|4 2 0⟩", -2.0)
- (fs"|3 3 0⟩", -1.7320508075688772)
-```
-Part of the [`AbstractHamiltonian`](@ref) interface.
-
-See also [`Offdiagonals`](@ref Main.Hamiltonians.Offdiagonals),
-[`AbstractOffdiagonals`](@ref Main.Hamiltonians.AbstractOffdiagonals).
-
-"""
-function offdiagonals(m::AbstractMatrix, i)
-    pairs = collect(zip(axes(m, 1), view(m, :, i)))
-    return filter!(pairs) do ((k, v))
-        k ≠ i && v ≠ 0
-    end
-end
-function offdiagonals(::AbstractOperatorColumn{A,T,O}) where {A,T,O}
-    if has_iterable_offdiagonals(O)
-        error("offdiagonals not implemented for operator type $O " *
-              "even though has_iterable_offdiagonals($O) == true")
-    else
-        throw(ArgumentError("offdiagonals not supported for operator type $O"))
-    end
-end
-
 @doc """
     LOStructure(op::AbstractHamiltonian)
     LOStructure(typeof(op))
@@ -496,6 +450,52 @@ starting_address(c::OffdiagonalsOperatorColumn) = c.address
 diagonal_element(c::OffdiagonalsOperatorColumn) = c.diagonal
 num_offdiagonals(c::OffdiagonalsOperatorColumn) = num_offdiagonals(c.operator, c.address)
 offdiagonals(c::OffdiagonalsOperatorColumn) = c.ods
+
+"""
+    offdiagonals(column)
+    offdiagonals(h::AbstractHamiltonian, address) # (deprecated)
+
+Return an iterator over nonzero off-diagonal matrix elements of `h` in the same column as
+`address`. Will iterate over pairs `(newaddress, matrixelement)` or `newaddress => matrixelement`.
+
+# Example
+
+```jldoctest
+julia> address = BoseFS(3,2,1);
+
+
+julia> H = HubbardReal1D(address);
+
+
+julia> h = offdiagonals(H, address)
+6-element Rimu.Hamiltonians.Offdiagonals{BoseFS{6, 3, BitString{8, 1, UInt8}}, Float64, HubbardReal1D{Float64, BoseFS{6, 3, BitString{8, 1, UInt8}}, 1.0, 1.0}}:
+ (fs"|2 3 1⟩", -3.0)
+ (fs"|2 2 2⟩", -2.449489742783178)
+ (fs"|3 1 2⟩", -2.0)
+ (fs"|4 1 1⟩", -2.8284271247461903)
+ (fs"|4 2 0⟩", -2.0)
+ (fs"|3 3 0⟩", -1.7320508075688772)
+```
+Part of the [`AbstractHamiltonian`](@ref) interface.
+
+See also [`Offdiagonals`](@ref Main.Hamiltonians.Offdiagonals),
+[`AbstractOffdiagonals`](@ref Main.Hamiltonians.AbstractOffdiagonals).
+
+"""
+function offdiagonals(m::AbstractMatrix, i)
+    pairs = collect(zip(axes(m, 1), view(m, :, i)))
+    return filter!(pairs) do ((k, v))
+        k ≠ i && v ≠ 0
+    end
+end
+function offdiagonals(::AbstractOperatorColumn{A,T,O}) where {A,T,O}
+    if has_iterable_offdiagonals(O)
+        error("offdiagonals not implemented for operator type $O " *
+              "even though has_iterable_offdiagonals($O) == true")
+    else
+        throw(ArgumentError("offdiagonals not supported for operator type $O"))
+    end
+end
 
 """
     random_offdiagonal(column)
