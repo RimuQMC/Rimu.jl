5b7fa64f658be7e790559984d8901d5e6d00f077
Satyanand Kuwar <168614969+Skuwar1@users.noreply.github.com>
2025-08-09 11:43:27 +1200

    Update HubbardRealSpace.jl

--- src/Hamiltonians/HubbardRealSpace.jl
+++ src/Hamiltonians/HubbardRealSpace.jl
@@ -110,18 +110,18 @@ Sum the local interactions of the Fock state `a` with all states in `bs` using t
 and nearest neighbour interaction constants in `us` and `Δs`. This is used to compute 
 all interactions in the column below the diagonal of the interaction matrix. 
 """
-@inline _interaction_col(a, ::Tuple{}, ::Tuple{}, ::Tuple{},_,_, ::CubicGrid) = 0
+@inline _interaction_col(a, ::Tuple{}, ::Tuple{}, ::Tuple{}, ::ModeMap, ::Tuple{}, ::CubicGrid) = 0
 @inline function _interaction_col(a, (b, bs...), (u, us...), (Δ, Δs...), occ_a, (occ_b, occs...), g::CubicGrid)
     return local_interaction(a, b, u, occ_a, occ_b) + _interaction_col(a, bs, us, Δs, occ_a, occs, g) +
         nearest_neighbour_interaction(onr(a), onr(b), Δ, g, occ_a)
 end
-@inline _interaction_col(a, ::Tuple{}, ::Tuple{}, ::Nothing,_,_, ::CubicGrid) = 0
+@inline _interaction_col(a, ::Tuple{}, ::Tuple{}, ::Nothing, ::ModeMap, ::Tuple{}, ::CubicGrid) = 0
 @inline function _interaction_col(a, (b, bs...), (u, us...), Δ::Nothing, g::CubicGrid)
-    return local_interaction(a, b, u, occ_a, occ_b) + _interaction_col(a, bs, us, Δs, occ_a, occs, g)
+    return local_interaction(a, b, u, occ_a, occ_b) + _interaction_col(a, bs, us, Δ, occ_a, occs, g)
 end
-@inline _interaction_col(a, ::Tuple{}, ::Nothing, ::Tuple{},_,_, ::CubicGrid) = 0
+@inline _interaction_col(a, ::Tuple{}, ::Nothing, ::Tuple{}, ::ModeMap, ::Tuple{}, ::CubicGrid) = 0
 @inline function _interaction_col(a, (b, bs...), u::Nothing, (Δ, Δs...), g::CubicGrid)
-    return _interaction_col(a, bs, us, Δs, occ_a, occs, g) +
+    return _interaction_col(a, bs, u, Δs, occ_a, occs, g) +
         nearest_neighbour_interaction(onr(a), onr(b), Δ, g, occ_a)
 end
 """
