f54d65a8f3ce3151dfe1848b815d61e23b52a3a1
Satyanand Kuwar <168614969+Skuwar1@users.noreply.github.com>
2025-08-10 12:13:01 +1200

    Update HubbardRealSpace.jl

--- src/Hamiltonians/HubbardRealSpace.jl
+++ src/Hamiltonians/HubbardRealSpace.jl
@@ -103,23 +103,20 @@ end
 
 """
     _interaction_col(a, bs::Tuple, us::Tuple, Δs::Tuple, occ::ModeMap, occs::Tuple, geometry::CubicGrid)
-    _interaction_col(a, bs::Tuple, us::Tuple, Δs::Nothing, occ::ModeMap, occs::Tuple, geometry::CubicGrid)
-    _interaction_col(a, bs::Tuple, us::Nothing, Δs::Tuple, occ::ModeMap, occs::Tuple, geometry::CubicGrid)
 
 Sum the local interactions of the Fock state `a` with all states in `bs` using the onsite 
 and nearest neighbour interaction constants in `us` and `Δs`. This is used to compute 
 all interactions in the column below the diagonal of the interaction matrix. 
 """
-@inline _interaction_col(a, ::Tuple{}, ::Tuple{}, ::Tuple{}, ::ModeMap, ::Tuple{}, ::CubicGrid) = 0
+@inline _interaction_col(a, ::Tuple{}, ::Union{Tuple{}, Tuple{Nothing}}, ::Union{Tuple{}, Tuple{Nothing}},
+    ::ModeMap, ::Tuple{}, ::CubicGrid) = 0
 @inline function _interaction_col(a, (b, bs...), (u, us...), (Δ, Δs...), occ_a, (occ_b, occs...), g::CubicGrid)
     return local_interaction(a, b, u, occ_a, occ_b) + _interaction_col(a, bs, us, Δs, occ_a, occs, g) +
         nearest_neighbour_interaction(onr(a), onr(b), Δ, g, occ_a)
 end
-@inline _interaction_col(a, ::Tuple{}, ::Tuple{}, ::Tuple{Nothing}, ::ModeMap, ::Tuple{}, ::CubicGrid) = 0
 @inline function _interaction_col(a, (b, bs...), (u, us...), Δ::Tuple{Nothing}, occ_a, (occ_b, occs...), g::CubicGrid)
     return local_interaction(a, b, u, occ_a, occ_b) + _interaction_col(a, bs, us, Δ, occ_a, occs, g)
 end
-@inline _interaction_col(a, ::Tuple{}, ::Tuple{Nothing}, ::Tuple{}, ::ModeMap, ::Tuple{}, ::CubicGrid) = 0
 @inline function _interaction_col(a, (b, bs...), u::Tuple{Nothing}, (Δ, Δs...), occ_a, (occ_b, occs...), g::CubicGrid)
     return _interaction_col(a, bs, u, Δs, occ_a, occs, g) +
         nearest_neighbour_interaction(onr(a), onr(b), Δ, g, occ_a)
