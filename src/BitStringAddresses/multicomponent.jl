8e733c1b057bb06a5defb1123c035b1aaa394111
Joachim Brand <joachim.brand@gmail.com>
2025-09-21 00:21:55 +0200

    Small fix in documentation (#351)

--- src/BitStringAddresses/multicomponent.jl
+++ src/BitStringAddresses/multicomponent.jl
@@ -2,7 +2,7 @@
     CompositeFS(addresses::SingleComponentFockAddress...) <: AbstractFockAddress
 
 Used to encode addresses for multi-component models. All component addresses
-are expected have the same number of modes.
+are expected to have the same number of modes.
 
 See also: [`BoseFS`](@ref), [`FermiFS`](@ref), [`SingleComponentFockAddress`](@ref),
 [`num_modes`](@ref), [`FermiFS2C`](@ref), [`AbstractFockAddress`](@ref).
