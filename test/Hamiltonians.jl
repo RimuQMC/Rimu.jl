2a7b9b93a9e9e306c4513a1b2a7ffa4a256db056
Satyanand Kuwar <168614969+Skuwar1@users.noreply.github.com>
2025-12-09 13:48:39 +1300

    Update Hamiltonians.jl

--- test/Hamiltonians.jl
+++ test/Hamiltonians.jl
@@ -34,7 +34,6 @@ end
         ExtendedHubbardReal1D(BoseFS(1, 0, 2, 1); t=1 + 0.5im),
         ExtendedHubbardReal1D(BoseFS(1, 0, 2, 1); t=2.0, power=3),
         ExtendedHubbardMom1D(BoseFS((1, 0, 0, 0, 1)); u=1.0, v=2.0, t=3.0),
-        ExtendedHubbardMom1D(BoseFS(1, 0, 2, 1); u=1 + 0.5im),
         ExtendedHubbardMom1D(BoseFS(1, 0, 2, 1); t=1 + 0.5im),
         ExtendedHubbardMom1D(OccupationNumberFS(1,2,0,0); u=1.0, v=2.0, t=3.0),
         ExtendedHubbardMom1D(FermiFS(1,1,0,0); u=1.0, v=2.0, t=3.0),
@@ -1979,6 +1978,8 @@ end
         @test round.(eigvals(Matrix(HM_f)), digits=8) ⊆ round.(eigvals(Matrix(HR_f)), digits=8)
         @test round.(eigvals(Matrix(HM_b)), digits=8) ⊆ round.(eigvals(Matrix(HR_b)), digits=8)
     end
+    @test_throws ArgumentError ExtendedHubbardMom1D(BoseFS(1,1,1,1); u = im)
+    @test_throws ArgumentError ExtendedHubbardMom1D(BoseFS(1,1,1,1); v = im)
 end
 
 @testset "ReducedDensityMatrix" begin
