8ac920b9ee11fd153f13122c7e3aa51191566a37
Satyanand Kuwar <168614969+Skuwar1@users.noreply.github.com>
2026-01-16 17:29:23 +1300

    Update global_step_action_test.jl

--- test/global_step_action_test.jl
+++ test/global_step_action_test.jl
@@ -52,7 +52,7 @@ end
     @testset "ParticleDensityGradientOverlaps" begin
         oops = ParticleDensityGradientOverlap((TestTwoParticleDensity,
             TwoParticleDensityGradient); name=(:gradient_test_overlaps,
-            :coefficient_vector_overlaps), test_vector_function = nothing, parameter)
+            :coefficient_vector_overlaps), testfunction = nothing, parameter)
 
         p = ProjectorMonteCarloProblem(h; n_replicas=3, global_step_actions=(oops,))
         res = solve(p)
@@ -74,7 +74,7 @@ end
     @testset "OverlapwithOptimization " begin
         gop = ParticleDensityGradientOverlap((TestTwoParticleDensity,
             TwoParticleDensityGradient); name=(:gradient_test_overlaps,
-            :coefficient_vector_overlaps), test_vector_function = nothing, parameter)
+            :coefficient_vector_overlaps), testfunction = nothing, parameter)
         oops = OverlapwithOptimization(gop; name = :parameter, step = 5, 
             threshold = 1e-2)
 
