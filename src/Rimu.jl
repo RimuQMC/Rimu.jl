932569356057e625b7d9309e037d491893fed8fd
Satyanand Kuwar <168614969+Skuwar1@users.noreply.github.com>
2026-01-16 15:30:50 +1300

    Add missing imports to Rimu.jl

--- src/Rimu.jl
+++ src/Rimu.jl
@@ -16,7 +16,7 @@ using Logging: ConsoleLogger
 using OrderedCollections: freeze
 using Random: Random, RandomDevice, seed!
 using NamedTupleTools: NamedTupleTools, namedtuple, delete
-using Optimisers: Adam, RAdam, destructure, Restructure
+using Optimisers: Adam, RAdam, destructure, Restructure,
     setup, update
 import Tables
 import ConsoleProgressMonitor
