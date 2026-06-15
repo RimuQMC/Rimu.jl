7b8089b7bcfe876f9e027810deb604994b643d43
Joachim Brand <joachim.brand@gmail.com>
2026-06-15 22:36:44 +1200

    combine function definitions for FermiFS and HardcoreBoseFS

--- src/BitStringAddresses/fermifs.jl
+++ src/BitStringAddresses/fermifs.jl
@@ -125,86 +125,13 @@ function print_address(io::IO, f::FermiFS{N,M}; compact=false) where {N,M}
     end
 end
 
-Base.bitstring(a::FermiFS) = bitstring(a.bs)
-Base.isless(a::FermiFS, b::FermiFS) = isless(a.bs, b.bs)
-Base.hash(a::FermiFS,  h::UInt) = hash(a.bs, h)
-Base.:(==)(a::FermiFS, b::FermiFS) = a.bs == b.bs
-num_occupied_modes(::Union{FermiFS{N}, HardcoreBoseFS{N}}) where {N} = N
-occupied_modes(a::FermiFS{N,<:Any,S}) where {N,S} = FermiOccupiedModes{N,S}(a.bs)
-
-num_unoccupied_modes(::Union{FermiFS{N,M}, HardcoreBoseFS{N,M}}) where {N,M} = M - N
-unoccupied_modes(a::FermiFS{N,M,S}) where {N,M,S} = FermiUnoccupiedModes{M - N,S}(a.bs)
-
-"""
-    unoccupied_mode_map(addr::Union{FermiFS, HardcoreBoseFS}) <: AbstractVector
-
-Get a map of unoccupied modes in [`FermiFS`](@ref) or [`HardcoreBoseFS`](@ref) address as an `AbstractVector`
-of indices compatible with [`excitation`](@ref).
-
-`unoccupied_mode_map(addr)[i]` contains the index for the `i`-th unoccupied mode.
-This is useful because unoccupied modes is required in some cases.
-`unoccupied_mode_map(addr)` is an eager version of the iterator returned by
-[`unoccupied_modes`](@ref). It is similar to [`onr`](@ref) but contains more information.
-
-Note that this function is only implemented for addresses of type [`FermiFS`](@ref).
-
-# Example
-
-```jldoctest
-julia> f = FermiFS(1,1,0,0)
-FermiFS{2,4}(1, 1, 0, 0)
-
-julia> mf = unoccupied_mode_map(f)
-2-element Rimu.BitStringAddresses.ModeMap{2, FermiFSIndex}:
- FermiFSIndex(occnum=0, mode=3, offset=2)
- FermiFSIndex(occnum=0, mode=4, offset=3)
-
-julia> mf == collect(unoccupied_modes(f))
-true
-
-```
-See also [`occupied_mode_map`](@ref).
-"""
-function unoccupied_mode_map(addr::Union{FermiFS{N,M}, HardcoreBoseFS{N,M}}) where {N,M}
-    modes = unoccupied_modes(addr)
-    T = eltype(modes)
-    L = num_unoccupied_modes(addr)
-    indices = MVector{L,T}(undef)
-    i = 0
-    for index in modes
-        i += 1
-        @inbounds indices[i] = index
-    end
-    return ModeMap(SVector(indices), i)
-end
-
 function near_uniform(::Type{FermiFS{N,M}}) where {N,M}
     return FermiFS([fill(1, N); fill(0, M - N)])
 end
 
-@inline function onr(a::FermiFS{<:Any,M}) where {M}
-    result = zero(MVector{M,Int32})
-    @inbounds for (_, mode) in occupied_modes(a)
-        result[mode] = 1
-    end
-    return SVector(result)
-end
-
-find_mode(a::FermiFS, i, occ=nothing) = fermi_find_mode(a.bs, i)
-
-function find_occupied_mode(a::FermiFS, i::Integer)
-    for k in occupied_modes(a)
-        i -= 1
-        i == 0 && return k
-    end
-    return FermiFSIndex(0, 0, 0)
-end
-
-function Base.reverse(f::FermiFS)
-    return typeof(f)(reverse(f.bs))
-end
-
 function excitation(a::FermiFS{N,M,S}, creations, destructions) where {N,M,S}
     new_bs, value = fermi_excitation(a.bs, creations, destructions)
-    return FermiFS{N,M,S}(new_bs), value
+    return FermiFS{N,M,S}(new_bs), value # carries sign, different from HardcoreBoseFS
 end
+
+# See "hardcorebosefs.jl" for other function definitions for FermiFS.
--- src/BitStringAddresses/hardcorebosefs.jl
+++ src/BitStringAddresses/hardcorebosefs.jl
@@ -110,17 +110,26 @@ function print_address(io::IO, f::HardcoreBoseFS{N,M}; compact=false) where {N,M
     end
 end
 
-Base.bitstring(a::HardcoreBoseFS) = bitstring(a.bs)
-Base.isless(a::HardcoreBoseFS, b::HardcoreBoseFS) = isless(a.bs, b.bs)
-Base.hash(a::HardcoreBoseFS,  h::UInt) = hash(a.bs, h)
-Base.:(==)(a::HardcoreBoseFS, b::HardcoreBoseFS) = a.bs == b.bs
-occupied_modes(a::HardcoreBoseFS{N,<:Any,S}) where {N,S} = FermiOccupiedModes{N,S}(a.bs)
+function excitation(a::HardcoreBoseFS{N,M,S}, creations, destructions) where {N,M,S}
+    new_bs, value = fermi_excitation(a.bs, creations, destructions)
+    return HardcoreBoseFS{N,M,S}(new_bs), abs(value) # different from FermiFS, no sign
+end
 
-unoccupied_modes(a::HardcoreBoseFS{N,M,S}) where {N,M,S} = FermiUnoccupiedModes{M - N,S}(a.bs)
+# joint functions for FermiFS and HardcoreBoseFS
+const FermiOrHardcoreBoseFS{N,M,S} = Union{FermiFS{N,M,S}, HardcoreBoseFS{N,M,S}}
 
-find_mode(a::HardcoreBoseFS, i, occ=nothing) = fermi_find_mode(a.bs, i)
+Base.bitstring(a::FermiOrHardcoreBoseFS) = bitstring(a.bs)
+Base.isless(a::F, b::F) where {F <: FermiOrHardcoreBoseFS} = isless(a.bs, b.bs)
+Base.hash(a::FermiOrHardcoreBoseFS, h::UInt) = hash(a.bs, h)
+Base.:(==)(a::F, b::F) where {F<:FermiOrHardcoreBoseFS} = a.bs == b.bs
 
-@inline function onr(a::HardcoreBoseFS{<:Any,M}) where {M}
+num_occupied_modes(::FermiOrHardcoreBoseFS{N}) where {N} = N
+num_unoccupied_modes(::FermiOrHardcoreBoseFS{N,M}) where {N,M} = M - N
+
+occupied_modes(a::FermiOrHardcoreBoseFS{N,<:Any,S}) where {N,S} = FermiOccupiedModes{N,S}(a.bs)
+unoccupied_modes(a::FermiOrHardcoreBoseFS{N,M,S}) where {N,M,S} = FermiUnoccupiedModes{M - N,S}(a.bs)
+
+@inline function onr(a::FermiOrHardcoreBoseFS{<:Any,M}) where {M}
     result = zero(MVector{M,Int32})
     @inbounds for (_, mode) in occupied_modes(a)
         result[mode] = 1
@@ -128,11 +137,59 @@ find_mode(a::HardcoreBoseFS, i, occ=nothing) = fermi_find_mode(a.bs, i)
     return SVector(result)
 end
 
-function Base.reverse(f::HardcoreBoseFS)
+find_mode(a::FermiOrHardcoreBoseFS, i, occ=nothing) = fermi_find_mode(a.bs, i)
+function find_occupied_mode(a::FermiOrHardcoreBoseFS, i::Integer)
+    for k in occupied_modes(a)
+        i -= 1
+        i == 0 && return k
+    end
+    return FermiFSIndex(0, 0, 0)
+end
+
+function Base.reverse(f::FermiOrHardcoreBoseFS)
     return typeof(f)(reverse(f.bs))
 end
 
-function excitation(a::HardcoreBoseFS{N,M,S}, creations, destructions) where {N,M,S}
-    new_bs, value = fermi_excitation(a.bs, creations, destructions)
-    return HardcoreBoseFS{N,M,S}(new_bs), abs(value)
+"""
+    unoccupied_mode_map(address::Union{FermiFS, HardcoreBoseFS}) <: AbstractVector
+
+Get a map of unoccupied modes in `address` as an `AbstractVector` of indices compatible
+with [`excitation`](@ref).
+
+`unoccupied_mode_map(address)[i]` contains the index for the `i`-th unoccupied mode.
+This is useful because unoccupied modes is required in some cases.
+`unoccupied_mode_map(address)` is an eager version of the iterator returned by
+[`unoccupied_modes`](@ref). It is similar to [`onr`](@ref) but contains more information.
+
+Note that this function is only implemented for addresses of type [`FermiFS`](@ref) and
+[`HardcoreBoseFS`](@ref).
+
+# Example
+
+```jldoctest
+julia> f = FermiFS(1,1,0,0)
+FermiFS{2,4}(1, 1, 0, 0)
+
+julia> mf = unoccupied_mode_map(f)
+2-element Rimu.BitStringAddresses.ModeMap{2, FermiFSIndex}:
+ FermiFSIndex(occnum=0, mode=3, offset=2)
+ FermiFSIndex(occnum=0, mode=4, offset=3)
+
+julia> mf == collect(unoccupied_modes(f))
+true
+
+```
+See also [`occupied_mode_map`](@ref).
+"""
+function unoccupied_mode_map(addr::FermiOrHardcoreBoseFS{N,M}) where {N,M}
+    modes = unoccupied_modes(addr)
+    T = eltype(modes)
+    L = num_unoccupied_modes(addr)
+    indices = MVector{L,T}(undef)
+    i = 0
+    for index in modes
+        i += 1
+        @inbounds indices[i] = index
+    end
+    return ModeMap(SVector(indices), i)
 end
