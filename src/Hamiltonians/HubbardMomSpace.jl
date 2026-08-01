7eefbc662329502612e58cd08927f8f9af5e4fdc
Satyanand Kuwar <168614969+Skuwar1@users.noreply.github.com>
2026-08-01 22:57:13 +1200

    Update src/Hamiltonians/HubbardMomSpace.jl
    Co-authored-by: Joachim Brand <joachim.brand@gmail.com>

--- src/Hamiltonians/HubbardMomSpace.jl
+++ src/Hamiltonians/HubbardMomSpace.jl
@@ -320,8 +320,8 @@ in `D` dimensions and with a total of `M` momentum modes.
   \\hat{H} = -\\sum_{k,σ} ϵ_{kσ} n_{kσ} +
   \\sum_{p,q,k,σ,σ'} V_{σσ'} a^†_{p+k,σ} a^†_{q-k,σ'} a_{q,σ'} a_{p,σ}
 ```
-where ``ϵ_{kσ} = -2 (\\sum_{d=1}^{D} \\Re(t_{σ,d}) \\cos(k_d) + \\Im(t_{σ,d}) \\sin(k_d))`` is 
-the single-particle `dispersion` and
+where ``ϵ_{kσ} = Σ_{d=1}^{D} ε(k_d)`` and ``ε(k)`` is a one-dimensional single particle `dispersion` 
+(with default [`hubbard_dispersion`](@ref))  and
 ``V_{σσ'} = (u_{σσ'}(1- \\frac{δ_{σσ'}}{2}) + w_{σσ'} \\sum_{d=1}^{D} \\cos(q_d))/M``
 the coefficients of a two-body interaction with onsite (``u_{σσ'}``) and nearest-neighbour 
 (``w_{σσ'}``) interaction terms.
